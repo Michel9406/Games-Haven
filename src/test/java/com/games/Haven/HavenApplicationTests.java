e3ae8cafc9b9fac8600f068f2fca530c45735bf8
Michel9406 <michel9406@hotmail.com>
2024-07-10 14:15:27 -0300

    model

---
+++ src/test/java/com/games/Haven/HavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.Haven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
