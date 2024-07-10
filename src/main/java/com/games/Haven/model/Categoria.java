8ac71c376ea12469f431aa49b32bac8a25028df7
Michel9406 <michel9406@hotmail.com>
2024-07-10 14:19:27 -0300

    adicionando package model

---
+++ src/main/java/com/games/Haven/model/Categoria.java
@@ -0,0 +1,56 @@
+package com.games.Haven.model;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo Descrição é obrigatório")
+	private String tipo;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Produtos> categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Produtos> getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(List<Produtos> categoria) {
+		this.categoria = categoria;
+	}
+
+
+}
