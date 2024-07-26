0fefadda74deaeb1800f81ca18d9883f1025832e
Michel9406 <michel9406@hotmail.com>
2024-07-25 21:25:41 -0300

    modifando

--- src/main/java/com/games/Haven/HavenApplication.java
+++ src/main/java/com/games/Haven/HavenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HavenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HavenApplication.class, args);
+    SpringApplication.run(HavenApplication.class, args);
 	}
 
 }
